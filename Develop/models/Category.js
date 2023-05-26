6adfe84e9afca76c29e79a60cc584d975678e1f9
abc <abc@example.com>
2023-05-25 23:15:55 -0400

    got it working for the most part, need to figure out insomnia now

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -8,7 +8,7 @@ Category.init(
   {
     // define columns
     id: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -37,15 +37,7 @@ router.post('/', async (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-    try {
-      const newProduct = await Product.create(req.body);
-      res.status(200).json(newProduct);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  },
-
-  Product.create(req.body)
+    Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -64,7 +56,8 @@ router.post('/', async (req, res) => {
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
-    }),
+    });
+  });
 
 // update product
 router.put('/:id', (req, res) => {
@@ -106,7 +99,7 @@ router.put('/:id', (req, res) => {
       // console.log(err);
       res.status(400).json(err);
     });
-}),
+});
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
@@ -126,6 +119,6 @@ router.delete('/:id', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-}),
+});
 
 module.exports = router;
